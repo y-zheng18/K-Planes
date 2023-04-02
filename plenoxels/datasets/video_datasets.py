d4d291bd5ba809a3e79b47272019189f0f85a7f2
y-zheng18 <yzheng18@stanford.edu>
2023-04-02 12:48:58 -0700

    save separate files

--- plenoxels/datasets/video_datasets.py
+++ plenoxels/datasets/video_datasets.py
@@ -402,7 +402,7 @@ def load_llffvideo_poses(datadir: str,
     if split == 'train':
         split_ids = np.arange(1, poses.shape[0])
     elif split == 'test':
-        split_ids = np.array([0, 1])
+        split_ids = np.array([1])
     else:
         split_ids = np.arange(poses.shape[0])
     if 'coffee_martini' in datadir:

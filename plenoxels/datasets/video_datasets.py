dac9b49ff42ebbce057649f1fb2c6e244793ef1e
y-zheng18 <yzheng18@stanford.edu>
2023-04-02 22:20:47 -0700

    debug

--- plenoxels/datasets/video_datasets.py
+++ plenoxels/datasets/video_datasets.py
@@ -473,7 +473,7 @@ def load_llffvideo_poses(datadir: str,
     if split == 'train':
         split_ids = np.arange(1, poses.shape[0])
     elif split == 'test':
-        split_ids = np.array([0])
+        split_ids = np.array([2])
     else:
         split_ids = np.arange(poses.shape[0])
     if 'coffee_martini' in datadir:

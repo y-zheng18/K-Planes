acf0ff1185b90e6f8c71dca1819bb8d5b7d8363c
y-zheng18 <yzheng18@stanford.edu>
2023-04-02 21:29:27 -0700

    debug

--- plenoxels/datasets/video_datasets.py
+++ plenoxels/datasets/video_datasets.py
@@ -95,11 +95,8 @@ class Video360Dataset(BaseDataset):
                     split='train', keyframes=keyframes, keyframes_take_each=30)
                 self.poses = poses.float()[0].repeat(120, 1, 1)
                 print('poses', self.poses.shape)
-                if contraction:
-                    self.per_cam_near_fars = per_cam_near_fars.float()[0][0]
-                else:
-                    self.per_cam_near_fars = torch.tensor(
-                        [[0.0, self.ndc_far]]).repeat(per_cam_near_fars.shape[0], 1)
+                self.per_cam_near_fars = per_cam_near_fars.float()[0][0]
+
                 print('per_cam_near_fars', self.per_cam_near_fars.shape)
                 timestamps = (timestamps.float() / 119) * 2 - 1
                 imgs = None

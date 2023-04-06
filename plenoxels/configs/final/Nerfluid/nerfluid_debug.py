13e5577b7f52ecba6d9225cafd9165d3b41c4571
y-zheng18 <yzheng18@stanford.edu>
2023-04-06 14:17:35 -0700

    debug

--- plenoxels/configs/final/Nerfluid/nerfluid_debug.py
+++ plenoxels/configs/final/Nerfluid/nerfluid_debug.py
@@ -11,7 +11,7 @@ config = {
  'isg_step': -1,
  'ist_step': -1,
  'keyframes': False,
- 'scene_bbox': [[0, 0, -0.2], [2, 2, 2]],
+ 'scene_bbox': [[0.5, 0.5, -0.2], [1.8, 1.8, 2]],
 
  # Optimization settings
  'num_steps': 30001,
